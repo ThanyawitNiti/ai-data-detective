1cd248fd1ebff50814848ca8ea8ece6b47e406d4
Thanyawit_[Palm] <124046836+ThanyawitNiti@users.noreply.github.com>
2026-09-02 21:57:38 +0700

    feat: export tool calling interface

--- src/ai_data_detective/__init__.py
+++ src/ai_data_detective/__init__.py
@@ -1,6 +1,14 @@
 """AI Data Detective package."""
 
 from ai_data_detective.market_data import load_market_data
+from ai_data_detective.tool_calling import ToolCallingResult, answer_question
 from ai_data_detective.tools import QueryValidationError, get_schema, run_sql
 
-__all__ = ["QueryValidationError", "get_schema", "load_market_data", "run_sql"]
+__all__ = [
+    "QueryValidationError",
+    "ToolCallingResult",
+    "answer_question",
+    "get_schema",
+    "load_market_data",
+    "run_sql",
+]
